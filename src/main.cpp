40c1ab3246c09ebbee21826f0974574463b10796
Roma <Roma@10202.local>
2018-03-15 13:58:28 +0300

    Hello Triangle completed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "glad.h"
 #include "glfw3.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
+int readFromFile(const std::string path, char **src, int *size);
+
+float vertices[] = {
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f
+};
+
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
 
 int main()
 {
@@ -32,14 +48,92 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    char *vSrc, *fSrc;
+    int size;
+    if(readFromFile("./res/shaders/vertex.shader", &vSrc, &size) == 0)
+        return -1;
+    if(readFromFile("./res/shaders/fragment.shader", &fSrc, &size) == 0)
+        return -1;
+
+    unsigned int vertexShader, fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &vSrc, NULL);
+    glCompileShader(vertexShader);
+    delete vSrc;
+
+    glShaderSource(fragmentShader, 1, &fSrc, NULL);
+    glCompileShader(fragmentShader);
+    delete fSrc;
+
+    int successV, successF;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &successV);
+    if (!successV)
+    {
+        char infoLog[1024];
+        glGetShaderInfoLog(vertexShader, 1024, NULL, infoLog);
+        std::cout<<"SHADER COMPILATION ERROR: "<<infoLog<<std::endl;
+        glDeleteShader(vertexShader);
+        return -1;
+    }
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successF);
+    if (!successF)
+    {
+        char infoLog[1024];
+        glGetShaderInfoLog(fragmentShader, 1024, NULL, infoLog);
+        std::cout<<"SHADER COMPILATION ERROR: "<<infoLog<<std::endl;
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        return -1;
+    }
+
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[1024];
+        glGetProgramInfoLog(program, 1024, NULL, infoLog);
+        std::cout<<"PROGRAM LINKING ERROR: "<<infoLog<<std::endl;
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        glDeleteProgram(program);
+        return -1;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(program);
+
     glViewport(0, 0, 800, 600);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    unsigned int VBO, VAO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(program);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -57,4 +151,23 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+int readFromFile(const std::string path, char** src, int *size)
+{
+    std::ifstream in(path.c_str(), std::ios::in | std::ios::binary);
+    if (in)
+    {
+        in.seekg(0, std::ios::end);
+        int l_size = in.tellg();
+        in.seekg(0, std::ios::beg);
+        char* newSrc = new char[l_size+1];
+        in.read(newSrc, l_size);
+        in.close();
+        newSrc[l_size] = '\0';
+        *size = l_size;
+        *src = newSrc;
+        return l_size;
+    }
+    return 0;
 }
